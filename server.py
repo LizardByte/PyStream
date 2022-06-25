6f99ce72df5c019401bf22d35c82d46974b45470
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2022-06-25 15:11:03 -0400

    Use dxcam for caputre with fallback options
    - DXcam allows capture using Desktop Duplication API on Windows, which is roughly 3x faster than mss or Pillow on initial testing

--- server.py
+++ server.py
@@ -1,51 +1,78 @@
 # standard imports
 from socket import socket
 from threading import Thread
+import platform
 import time
 
 # lib imports
 from lz4.frame import compress
 from mss import mss
+import numpy as np
+from PIL import ImageGrab
+
+fallback = 'mss'
+monitor_number = 0
+
+# set up capture method
+if platform.system() == 'Windows':
+    import dxcam
+    camera = dxcam.create(max_buffer_len=512)  # returns a DXCamera instance on primary monitor
+else:
+    camera = None
+    sct = mss()
+
+    mon = sct.monitors[monitor_number]
+
+    # The region to capture
+    monitor = dict(
+        top=mon["top"],
+        left=mon["left"],
+        width=mon["width"],
+        height=mon["height"],
+        mon=monitor_number
+    )
+
+
+def image_capture():
+    frame = None
+    if camera:
+        frame = np.array(camera.get_latest_frame())
+    elif fallback == 'mss':
+        frame = sct.grab(monitor=monitor).rgb
+    elif fallback == 'pil':
+        frame = np.array(ImageGrab.grab())
+
+    return frame
 
 
 def retrieve_screenshot(conn):
-    with mss() as sct:
-        # I believe 0 and -1 are all monitors... need to confirm
-        monitor_number = 0
-        mon = sct.monitors[monitor_number]
 
-        # The region to capture
-        monitor = dict(
-            top=mon["top"],
-            left=mon["left"],
-            width=mon["width"],
-            height=mon["height"],
-            mon=monitor_number
-        )
+    while True:
+        last_time = time.perf_counter()
 
-        while "Screen capturing":
-            last_time = time.time()
+        # Capture the screen
+        frame = image_capture()
 
-            # use sct.grab to grab a portion of the screen
-            img = sct.grab(monitor=monitor)
+        if frame is None:
+            break
 
-            # Tweak the compression level here (0-9)
-            pixels = compress(img.rgb, 0)
+        # Tweak the compression level here (0-9)
+        pixels = compress(frame, 0)
 
-            # Send the size of the pixels length
-            size = len(pixels)
-            size_len = (size.bit_length() + 7) // 8
-            conn.send(bytes([size_len]))
+        # Send the size of the pixels length
+        size = len(pixels)
+        size_len = (size.bit_length() + 7) // 8
+        conn.send(bytes([size_len]))
 
-            # Send the actual pixels length
-            size_bytes = size.to_bytes(size_len, 'big')
-            conn.send(size_bytes)
+        # Send the actual pixels length
+        size_bytes = size.to_bytes(size_len, 'big')
+        conn.send(size_bytes)
 
-            # Send pixels
-            conn.sendall(pixels)
+        # Send pixels
+        conn.sendall(pixels)
 
-            # print fps to terminal
-            print(f"fps: {1 / (time.time() - last_time)}")
+        # print fps to terminal
+        print(f"{1 / (time.perf_counter() - last_time)}")
 
 
 def main(host='0.0.0.0', port=5000):
@@ -55,13 +82,17 @@ def main(host='0.0.0.0', port=5000):
         sock.listen(5)
         print('Server started.')
 
-        while 'connected':
+        while True:
             conn, addr = sock.accept()
             print('Client connected IP:', addr)
+            if camera:
+                camera.start(target_fps=140)
             thread = Thread(target=retrieve_screenshot, args=(conn,))
             thread.start()
     finally:
         sock.close()
+        if camera:
+            camera.stop()
 
 
 if __name__ == '__main__':
