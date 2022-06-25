074f7fda5da979dd14ad49909e12fd6f3637eb41
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2022-06-24 20:05:38 -0400

    Create client.py

---
+++ client.py
@@ -0,0 +1,60 @@
+# standard imports
+from socket import socket
+
+# lib imports
+from lz4.frame import decompress
+import pygame
+
+# this display to show on
+display = 0
+
+# todo - allow different resolution than server output
+WIDTH = 1920
+HEIGHT = 1080
+
+
+def recv_all(conn, length):
+    """ Retreive all pixels. """
+
+    buf = b''
+    while len(buf) < length:
+        data = conn.recv(length - len(buf))
+        if not data:
+            return data
+        buf += data
+    return buf
+
+
+def main(host='127.0.0.1', port=5000):
+    pygame.init()
+    screen = pygame.display.set_mode(size=(WIDTH, HEIGHT), display=display)
+    clock = pygame.time.Clock()
+    watching = True
+
+    sock = socket()
+    sock.connect((host, port))
+    try:
+        while watching:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    watching = False
+                    break
+
+            # Retrieve the size of the pixels length, the pixels length and pixels
+            size_len = int.from_bytes(sock.recv(1), byteorder='big')
+            size = int.from_bytes(sock.recv(size_len), byteorder='big')
+            pixels = decompress(recv_all(sock, size))
+
+            # Create the Surface from raw pixels
+            img = pygame.image.fromstring(pixels, (WIDTH, HEIGHT), 'RGB')
+
+            # Display the picture
+            screen.blit(img, (0, 0))
+            pygame.display.flip()
+            clock.tick(60)
+    finally:
+        sock.close()
+
+
+if __name__ == '__main__':
+    main()
